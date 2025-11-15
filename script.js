bf1758c29016a1f04514d902a5fb0344a191e66b
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2025-11-15 22:45:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counterElement = document.getElementById("counter");
+
+document.getElementById("incrementBtn").onclick = function () {
+	let currentValue = parseInt(counterElement.textContent);
+
+	alert(currentValue);
+
+	counterElement.textContent = currentValue + 1;
+}
